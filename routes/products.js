cc7a294c844309ff0cbe8805ce2ea12f6d5a0eb1
mk2429 <mayank.20429@knit.ac.in>
2026-05-18 14:24:56 +0530

    new we

--- routes/products.js
+++ routes/products.js
@@ -9,7 +9,97 @@ router.get("/", async (req, res) => {
   const products = await Product.find({ isActive: true });
   res.json(products);
 });
+/* HOME PRODUCTS */
 
+router.get("/featured", async (req, res) => {
+
+  try {
+
+    const categories = [
+
+      "Ring",
+      "Pendants",
+      "Studs",
+      "Bracelets"
+    ];
+
+    let homeProducts = [];
+
+    for (const category of categories) {
+
+      const products =
+        await Product.find({
+
+          category,
+
+          isActive: true
+        })
+
+        .sort({ createdAt: -1 })
+
+        .limit(4);
+
+      homeProducts.push(...products);
+    }
+
+    res.json(homeProducts);
+
+  } catch (err) {
+
+    res.status(500).json({
+
+      message:
+        "Failed to fetch home products"
+    });
+  }
+});
+router.get("/paginated", async (req, res) => {
+
+  try {
+
+    const page =
+      Number(req.query.page) || 1;
+
+    const limit =
+      Number(req.query.limit) || 12;
+
+    const skip =
+      (page - 1) * limit;
+
+    const total =
+      await Product.countDocuments({
+        isActive: true
+      });
+
+    const products =
+      await Product.find({
+        isActive: true
+      })
+
+      .sort({ createdAt: -1 })
+
+      .skip(skip)
+
+      .limit(limit);
+
+    res.json({
+
+      products,
+
+      hasMore:
+        total >
+        skip + products.length
+    });
+
+  } catch (err) {
+
+    res.status(500).json({
+
+      message:
+        "Failed to fetch products"
+    });
+  }
+});
 router.get("/slug/:slug", async (req, res) => {
   const product = await Product.findOne({
     slug: req.params.slug,
@@ -46,5 +136,7 @@ router.delete("/:id", auth, async (req, res) => {
   await Product.findByIdAndDelete(req.params.id);
   res.json({ message: "Deleted" });
 });
+/* PAGINATED PRODUCTS */
+
 
 module.exports = router;
