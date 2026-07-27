08b9c650f7bf626e86b87f4778355900ae93724a
mk2429 <mayank.20429@knit.ac.in>
2026-07-27 12:48:56 +0530

    mail issue

--- routes/orders.js
+++ routes/orders.js
@@ -6,20 +6,16 @@ const router = express.Router();
 const Order = require("../models/Order");
 const nodemailer =
     require("nodemailer");
-const transporter =
-    nodemailer.createTransport({
-
-        service: "gmail",
-
-        auth: {
-
-            user:
-                process.env.EMAIL_USER,
-
-            pass:
-                process.env.EMAIL_PASS
-        }
-    });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4, // Force IPv4
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 /* =====================================
    SHIPROCKET LOGIN
 ===================================== */
